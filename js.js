1d39785133966c0e8a41218633dafa815cac41c1
maxbert <mbertfield@stuy.edu>
2017-03-09 14:52:36 -0500

    done

--- js.js
+++ js.js
@@ -3,9 +3,10 @@ var h = window.innerHeight * 0.9;
 var svg = document.getElementsByTagName('svg')[0];
 svg.setAttribute("height",h);
 svg.setAttribute("width",w);
-var createcirc = function(e){
 
-    
+var moving = false;
+
+var createcirc = function(e){
     //svg.appendChild(c);
     var c = document.createElementNS("http://www.w3.org/2000/svg","circle");
     c.setAttribute("r","10");
@@ -28,6 +29,8 @@ var event = function(e){
 
 
 var addcircH = function(x, y){
+
+    // Creation
     if (x < 5 && y < 5){
 	x = Math.random() * w;
 	y = Math.random() * h;
@@ -38,7 +41,48 @@ var addcircH = function(x, y){
     d.setAttribute('cx', x);
     d.setAttribute('cy', y);
     svg.appendChild(d);
-    
+    var direction = false;
+    var ydirection = false;
+    var rand = Math.random() * 3
+    // Movement
+
+    var move = function() {
+	if (moving) {
+	    d.setAttribute("cy", y);
+	    d.setAttribute("cx", x);
+	    
+	    
+	    if (direction == false){
+		x++;
+	    }else{
+		x--;
+	    };
+
+	    if (ydirection == false){
+		y+=rand;
+	    }else{
+		y-=rand;
+	    };
+
+	    if (x >= w ){
+		direction = true;
+	    };
+	    if (x <= 0){
+		direction = false;
+	    };
+	    if (y >= h){
+		ydirection = true;
+	    };
+	    if (y <=  0){
+		ydirection = false;
+	    };
+	    //console.log(d);
+	    
+	};
+	window.requestAnimationFrame( move )
+
+    };
+    move();
 };
 var addcirc = function(e) {
     var x = e.offsetX;
@@ -55,3 +99,5 @@ var clearer = function(e){
     };
 };
 but.addEventListener('click', clearer);
+
+document.getElementById('move').addEventListener('click', function(e){moving = true;});
