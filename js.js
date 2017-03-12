180ee22ef9023d0eefe4dca0727a08a3fca4e224
Zzcnick <zzhen@stuy.edu>
2017-03-12 15:34:35 -0400

    fix

--- js.js
+++ js.js
@@ -20,7 +20,9 @@ var createcirc = function(e){
 var change = function(e) {
     if (this.getAttribute('fill') == 'green') {
 	addcircH(Math.random() * w,
-		 Math.random() * h);
+		 Math.random() * h,
+		 this.getAttribute("r"),
+		 Math.random() < 0.5 ? true : false);
 	this.parentElement.removeChild(this);
     } else {
 	this.setAttribute("fill",'green');
@@ -33,7 +35,7 @@ var addCirc = function(e) {
     var y = e.offsetY;
     addcircH(x, y, 50, false);    
 }
-var addcircH = function(x, y, r, d){
+var addcircH = function(x, y, r, dir){
     // Creation
     console.log(x);
     console.log(y);
@@ -43,40 +45,22 @@ var addcircH = function(x, y, r, d){
     d.setAttribute('cy', y);
     d.setAttribute('r', r);
     svg.appendChild(d);
-   
 	
-    var direction = d;
+    var direction = dir; // False means Right
     var ydirection = false;
-    var rand = Math.random() * 3 + 1
-    var randx = Math.random() * 3 + 1
-    // Movement
+    var rand = Math.random() * 3 + 1;
+    var randx = Math.random() * 3 + 1;
 
+    // Movement
     var move = function() {
 	if (moving) {
-	    var side = "left";
-	    if (x > w/2){
-		side = "right";
-	    };
 	    d.setAttribute("cy", y);
-	    d.setAttribute("cx", x);
+	    d.setAttribute("cx", x);	    
 	    
-	    
-	    if (direction == false){
-		x+=randx;
-	    }else{
-		x-=randx;
-	    };
-
-	    if (ydirection == false){
-		y+=rand;
-	    }else{
-		y-=rand;
-	    };
-
-	    if (x >= w ){
+	    if (x >= w ){ // Move Left 
 		direction = true;
 	    };
-	    if (x <= 0){
+	    if (x <= 0){ // Move Right
 		direction = false;
 	    };
 	    if (y >= h){
@@ -86,27 +70,41 @@ var addcircH = function(x, y, r, d){
 		ydirection = false;
 	    };
 
-	    if(x < w/2 && side == "right") {
+	    var side = x < w/2 ? "left" : "right";
+	    if (direction == false){
+		x+=randx;
+	    }else{
+		x-=randx;
+	    };
+	    if (ydirection == false){
+		y+=rand;
+	    }else{
+		y-=rand;
+	    };
+
+	    if (x < w/2 && side == "right") {
+		side = "left";
 		r /= 2;
 		d.setAttribute("r", r);
-		addcircH(x,y,r, false );
-	    };
-	    if( (x > w/2 && side == "left")) {
-		   r /= 2;
-		   d.setAttribute("r", r);
-		   addcircH(x,y,r, true);
-	       };
-	       
+		addcircH(x + randx,y,r,false);
+	    }
+	    else if (x > w/2 && side == "left") {
+		side = "right";
+		r /= 2;
+		d.setAttribute("r", r);
+		addcircH(x - randx,y,r,true);
+	    };	    
+	    
+	    if (r < 1) {
+		svg.removeChild(d);
+		return;
+	    }
 	};
 	window.requestAnimationFrame( move )
 
     };
     move();
 };
-
-
-
-
 
 svg.addEventListener('click', addCirc);
 var but = document.getElementById("clear");
